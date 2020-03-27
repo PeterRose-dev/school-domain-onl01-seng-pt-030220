6cd83701cb7ab415310204a7b18e70061aad9e38
Peter Rose <peterroseq1@gmail.com>
2020-03-27 00:29:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :roster; :student_name; :grade
+  attr_reader :name 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def roster
+    @roster = {} 
+  end
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+    @roster[grade].push [student_name]
+  end
+end
+school.add_student("Zach Morris", 9)
+school.roster
+binding.pry 
