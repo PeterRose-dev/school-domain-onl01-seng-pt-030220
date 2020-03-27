938e48b4e6f5dc5c9b2becbbf68ae170e8905c8b
Peter Rose <peterroseq1@gmail.com>
2020-03-27 00:29:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,31 @@
 require 'pry'
 class School
-  attr_accessor :roster; :student_name; :grade
-  attr_reader :name 
-  def initialize(name)
-    @name = name
+
+  def initialize(school_name)
+    @school_name = school_name
     @roster = {}
   end
+
   def roster
-    @roster = {} 
+    @roster
   end
-  def add_student(student_name, grade)
-    @student_name = student_name
+
+  def add_student(name, grade)
+    @student_name = name
     @grade = grade
-    @roster[grade].push [student_name]
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
   end
 end
-school.add_student("Zach Morris", 9)
-school.roster
-binding.pry 
